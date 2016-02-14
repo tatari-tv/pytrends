04438e71816f2c09547b7c4a4766bc7d8b92ecf5
Cedric Han <cedric@tumblr.com>
2016-02-14 13:59:20 -0800

    Allow digits in text type inferring regex

--- pytrends/pyGTrends.py
+++ pytrends/pyGTrends.py
@@ -200,7 +200,7 @@ def _infer_dtype(val):
         return 'int'
     elif re.match(r'[+-]?\d+%$', val):
         return 'pct'
-    elif re.match(r'[a-zA-Z ]+', val):
+    elif re.match(r'[\da-zA-Z ]+', val):
         return 'text'
     else:
         msg = "val={0} dtype not recognized".format(val)
